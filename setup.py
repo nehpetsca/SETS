b6e836685e5370b4662106ef58ff89c04a72dbe7
Stephen Hill <nehpets@zosimple.net>
2023-02-06 17:16:42 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='SETS',
+    version='20230206a',
+    packages=[''],
+    url='https://stobuilds.com/SETS/',
+    license='GNU General Public License v3.0',
+    author='',
+    author_email='',
+    description='A Star Trek Online build tool in Python'
+)
+
+if __name__ == "__main__":
+    setup()
